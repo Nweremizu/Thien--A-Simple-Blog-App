b40a6aae6556f71badb9595fa7fa3741e40044d4
Nweremizu <nweremizu.bruno@stu.cu.edu.ng>
2023-12-17 08:47:05 +0000

    let's go

--- config.py
+++ config.py
@@ -4,14 +4,43 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Define the config class
-class Config(object):
+class Config(object, ):
     # Defines the configuration variables
     # Secret key for the application
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # URI for the database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-        # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Database configuration
+    DB_ENGINE = os.getenv('DB_ENGINE', None)
+    DB_USERNAME = os.getenv('DB_USERNAME', None)
+    DB_PASS = os.getenv('DB_PASS', None)
+    DB_HOST = os.getenv('DB_HOST', None)
+    DB_NAME = os.getenv('DB_NAME', None)
+    
+    USE_SQLITE = True
+    
+    if DB_ENGINE and DB_NAME and DB_USERNAME:
+            try:
+
+                # Relational DBMS: PSQL, MySql
+                SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}/{}'.format(
+                    DB_ENGINE,
+                    DB_USERNAME,
+                    DB_PASS,
+                    DB_HOST,
+                    DB_NAME
+                )
+
+                USE_SQLITE = False
+
+            except Exception as e:
+
+                print('> Error: DBMS Exception: ' + str(e))
+                print('> Fallback to SQLite ')
+                
+    if USE_SQLITE:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, 'app.db')
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+            
 
     # Email server configuration
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
@@ -20,6 +49,3 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') # None
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') # None
     ADMINS = ['brunex7900@gmail.com']
-
-    # Pagination
-    POSTS_PER_PAGE = 3
