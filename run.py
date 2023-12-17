abed439364058852263fd987717e3e3b256da303
Nweremizu <nweremizu.bruno@stu.cu.edu.ng>
2023-12-17 10:41:46 +0000

    let's go

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
-from app import app,db
+from app import app,db,create_tables
 import os
 
 @app.shell_context_processor
 def make_shell_context():
     return {'app': app}
 
+create_tables()
 
 if __name__ == '__main__':
     app.run(debug=os.environ.get('DEBUG', False))
