7ad6cffbeceff278358b400cee7fbb4a3de2031f
Nweremizu <nweremizu.bruno@stu.cu.edu.ng>
2023-12-17 11:04:10 +0000

    let's go

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(240))
+    password_hash = db.Column(db.String(230))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(200))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
